e69c099968752931aa4391103d366073b16c02ae
William <convidadoc38@gmai.com>
2024-03-01 13:06:58 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -18,6 +21,8 @@ import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	private DepartmentService service;
 
@@ -46,6 +51,10 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if( entity == null ) {
@@ -57,12 +66,20 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}catch(DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 		
 	}
+	
+	private void notifyDataChangeListeners() {		
+		for( DataChangeListener listener : dataChangeListeners ) {
+			listener.onDataChanged();
+		}
+	}
+
 	//Este método pega os dados do formulário, instancia um objeto
 	//do tipo Department e retorna este objeto
 	private Department getFormData() {
---
+++ src/gui/listeners/DataChangeListener.java
@@ -0,0 +1,8 @@
+package gui.listeners;
+
+public interface DataChangeListener {
+	
+	//Este método será disparado quando houver alguma mudança
+	void onDataChanged();
+
+}
