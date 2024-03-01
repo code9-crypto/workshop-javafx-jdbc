39dd5019f405cfc2b22f46239cee404e8c1a65e7
William <convidadoc38@gmai.com>
2024-03-01 11:24:47 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -5,8 +5,21 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
-
+	
+	//Este método pega a referência da janela atual
+	//Mas com um paramêtro do tipo ActionEvent
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage)  ((Node) event.getSource()).getScene().getWindow();
+	}
+	
+	//Este método converte um valor em inteiro
+	//Caso não seja possível fazer a conversão, retornará null
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}catch(NumberFormatException e) {
+			return null;
+		}
+		
 	}
 }
